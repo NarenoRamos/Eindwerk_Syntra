57550511fa7c6c89dfe5b3786c98e200c3f48b2e
NarenoRamos <giani.ramos@outlook.com>
2025-04-15 15:25:53 +0200

    comment app.py

--- server/app/app.py
+++ server/app/app.py
@@ -10,22 +10,25 @@ api_key=os.getenv('API_KEY')
 
 @app.route(f"/{api_key}", methods=["GET"])
 def get_routes():
-    date = request.args.get("date")  # Haal de datum op uit de URL
-    hour_str = request.args.get("hour")  # Haal het uur op uit de URL
+    #Retrive arg from api
+    date = request.args.get("date")  
+    hour_str = request.args.get("hour")  
     vehicle_class_str = request.args.get("vhclass")
+
 
     if hour_str is None:
         return {"error": f"Missing 'hour' parameter, {vehicle_class} {date} {hour_str}"}, 400  # Bad Request error
     try:
-        hour = int(hour_str)  # Zet uur om naar integer
+        #Convert args in correct format
+        hour = int(hour_str) 
         vehicle_class = int(vehicle_class_str)
     except ValueError:
-        return {"error": "'hour' must be an integer"}, 400  # Geef een foutmelding als het geen geldig getal is
+        return {"error": "args must be an integer"}, 400  #Bad request error 
     df = helpers.ring_with_speeds(vehicle_class, date, hour)
     if df.empty:
-        return jsonify({"error": "No data found for given date/hour"}), 400
+        return jsonify({"error": "No data found for given date/hour"}), 400 #Bad request error 
 
-    df = df[['route', 'path', 'actual_speed', 'speed_category']]
+    df = df[['route', 'path', 'actual_speed', 'speed_category']] 
     return df.to_dict(orient="records")
 
 if __name__ == '__main__':
