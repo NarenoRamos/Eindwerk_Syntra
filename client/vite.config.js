39ef91a4d4bcabe1733f88acec43cd94d5ff4626
NarenoRamos <giani.ramos@outlook.com>
2025-04-16 15:47:59 +0200

    change config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: ["*"], //change this to own dev domain
+    proxy: {
+      '/api': {
+        target: 'http://backend:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+    host: '0.0.0.0',
     port: 5173
-  }
-})
+  },
+});
